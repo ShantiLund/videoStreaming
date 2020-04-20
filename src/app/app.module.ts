7a569ca10b07dd489becef1b02f12a2aa6a54f1c
ShantiLund <shanti.cs15@iba-suk.edu.pk>
2020-04-20 12:15:02 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireStorageModule} from 'angularfire2/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDLXmfWdLxa48jgB5PV9Q0t0IwUNOs0dWc",
+      authDomain: "videostreaming-5c1bc.firebaseapp.com",
+      projectId: "videostreaming-5c1bc",
+      storageBucket: "videostreaming-5c1bc.appspot.com"
+    }),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
